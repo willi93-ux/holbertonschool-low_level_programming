0bab6c331f8629ac127c1ab637176281f0cf349f
William <Williamshilaire32@gmail.com>
2025-02-14 14:59:10 +0100

    dd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./4-puts_0
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
