8a85fcbacb5240654af3e06401bba418e3319964
William <Williamshilaire32@gmail.com>
2025-02-18 13:51:18 +0100

    updated

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,15 @@
-
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
- *
- * Return: 0
- **/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char c = 'a';
-do
-{
-if (c == 'e' || c == 'q')
-c++ ;
-putchar(c++);
-}
-while (c <= 'z');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'q' && ch != 'e')
+putchar(ch);
 putchar('\n');
 return (0);
 }
