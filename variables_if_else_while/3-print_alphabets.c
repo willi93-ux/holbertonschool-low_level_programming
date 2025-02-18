223291d0e87bff55b47e9f917e10b3cebc63eec8
willi93-ux <williamshilaire32@gmail.com>
2025-02-18 12:12:28 +0100

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase followed by a new line.
- * The program uses putchar to print each character.
+ * Description: Prints the alphabet in lowercase and uppercase followed
+ * by a new line. The program uses putchar to print each character.
  *
  * Return: Always 0 (Success)
  */
@@ -14,7 +14,8 @@ int main(void)
 
 	for (w = 'a'; w <= 'z'; w++)
 		putchar(w);
-  for (w = 'A'; w <= 'Z'; w++)
+	for (w = 'A'; w <= 'Z'; w++)
+		putchar(w);
 	putchar('\n');
 
 	return (0);
