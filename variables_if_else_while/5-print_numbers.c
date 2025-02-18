8798fcdee8a2a65b4fe2ed8883f01f0c30fe313d
willi93-ux <williamshilaire32@gmail.com>
2025-02-18 15:38:16 +0100

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * print_numbers - Prints the numbers from 0 to 9 followed by a new line
- *
+ * main - Entry point
  * Description: This function uses a loop to iterate through
  * the numbers from 0 to 9 and prints them to the screen.
  * It then adds a new line.
