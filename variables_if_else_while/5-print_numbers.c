d0e69fe38f28b3fab8ab5e3e41730f69df1457be
willi93-ux <williamshilaire32@gmail.com>
2025-02-18 15:33:33 +0100

    Update 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line
+ *
+ * Description: This function uses a loop to iterate through
+ * the numbers from 0 to 9 and prints them to the screen.
+ * It then adds a new line.
+ *
+ * Return: Does not return anything (void)
+ */
+
+int main (void)
+{
+  char numbers;
+for (numbers = '0'; numbers <= '9'; numbers++)
+{
+printf("%c", numbers);
+}
+printf("\n");
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
