86b814e5209c480353418d53228b2d044a4a66f4
William <Williamshilaire32@gmail.com>
2025-02-20 11:40:39 +0100

    dd

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+/**
+* main - Prints the numbers since 0 to 9
+*
+* Return: Always (Success)
+*/
+
 int main(void)
 {
-    int digit;
+int digit;
 
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0');
-    }
-    putchar('\n');
+for (digit = 0; digit <= 9; digit++)
+{
+putchar(digit + '0');
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
