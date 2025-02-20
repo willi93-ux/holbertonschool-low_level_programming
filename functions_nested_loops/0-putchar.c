e98d8eb9d139cb7378374c5accc7aeaecf9afa43
William <Williamshilaire32@gmail.com>
2025-02-20 14:51:54 +0100

    dd

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Write a program that prints _putchar, followed by a new line.
+* The program uses putchar to print each character.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
