a6c270f89c7b05014fbac04fc5fb541a4b0ebad0
William <Williamshilaire32@gmail.com>
2025-03-03 13:27:10 +0100

    dd

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,23 +6,32 @@
 #include "holberton.h"
 
 /**
- * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
+ * strncat - Concatenates two strings using at most
+ *           an inputted number of bytes from src.
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *strncat(char *dest, const char *src, int n)
 {
-	int index = 0, dest_len = 0;
+    int dest_len = 0, index = 0;
 
-	while (dest[index++])
-		dest_len++;
+    /* Find the length of dest */
+    while (dest[dest_len])
+        dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+    /* Append src to dest, using at most n bytes */
+    while (src[index] && index < n)
+    {
+        dest[dest_len] = src[index];
+        dest_len++;
+        index++;
+    }
 
-	return (dest);
+    /* Null-terminate the resulting string */
+    dest[dest_len] = '\0';
+
+    return dest;
 }
