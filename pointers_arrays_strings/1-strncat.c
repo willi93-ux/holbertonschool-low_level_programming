039940952b46aaf5323c545c7d456c0bba1fef52
William <Williamshilaire32@gmail.com>
2025-03-04 12:39:01 +0100

    dd

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,32 @@
-/*
- * File: 1-strncat.c
- * Auth: Brennan D Baraban
- */
-
 #include "holberton.h"
 #include <stdio.h>
 
 /**
- * strncat - Concatenates two strings using at most
- *           an inputted number of bytes from src.
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
+ * _strncat - function that concatenates two strings
  *
- * Return: A pointer to the resulting string dest.
+ * @dest: parameter defined in main
+ * @src: parameter defined in main
+ * @n: parameter defined in main
+ *
+ * Return: memory address of function (string)
  */
-char *strncat(char *dest, const char *src, int n)
+
+char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0, index = 0;
+	char *tmp1 = dest;
+	char *tmp2 = src;
 
-    /* Find the length of dest */
-    while (dest[dest_len])
-        dest_len++;
-
-    /* Append src to dest, using at most n bytes */
-    while (src[index] && index < n)
-    {
-        dest[dest_len] = src[index];
-        dest_len++;
-        index++;
-    }
-
-    /* Null-terminate the resulting string */
-    dest[dest_len] = '\0';
-
-    return dest;
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (src < tmp2 + n && *src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest++ = '\0';
+	dest = tmp1;
+	return (dest);
 }
