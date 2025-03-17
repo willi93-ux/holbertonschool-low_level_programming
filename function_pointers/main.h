531ec9414c76de8ecc9e39e0f9c740a010af30c3
William <Williamshilaire32@gmail.com>
2025-03-17 01:54:09 +0100

    dd

---
+++ function_pointers/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: function_pointers.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
